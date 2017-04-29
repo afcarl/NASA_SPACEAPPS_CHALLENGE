2cc2ac9852a1553a88cbf3b704e7ed2526013fad
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-29 15:45:21 +0530

    Add migrations for User table

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.TextField(null=True)
+    company_name = models.CharField(max_length=50)
+    address = models.TextField()
+    website = models.URLField(max_length=20, null=True, blank=True)

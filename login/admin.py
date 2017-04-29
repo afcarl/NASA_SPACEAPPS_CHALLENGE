9666895e989443ffd2ea212abe0f9e087bbd3241
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-29 16:33:34 +0530

    Add User and Company table

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User,Company
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','email','company_id']
+
+class ComapanyAdmin(admin.ModelAdmin):
+    list_display = ['id','company_name']
+
+
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Company,ComapanyAdmin)
--- login/models.py
+++ login/models.py
@@ -4,6 +4,12 @@ class User(models.Model):
     username = models.CharField(max_length=20)
     email = models.EmailField()
     password = models.TextField(null=True)
-    company_name = models.CharField(max_length=50)
-    address = models.TextField()
+    company_id = models.IntegerField(max_length =20, null = True)
+    address = models.TextField(null = True)
     website = models.URLField(max_length=20, null=True, blank=True)
+
+class Company(models.Model):
+    company_name = models.CharField(max_length = 50, null = True)
+
+class admin_table(models.Model):
+    pass
